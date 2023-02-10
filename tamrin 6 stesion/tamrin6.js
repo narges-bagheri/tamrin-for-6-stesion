3765b976f336b834d3ff3bd9a9c3e9f10c447d96
narges-bagheri <111405499+narges-bagheri@users.noreply.github.com>
2023-02-11 00:32:36 +0330

    Add files via upload

---
+++ tamrin 6 stesion/tamrin6.js
@@ -0,0 +1,30 @@
+//tamrin 1
+// const m=[]
+// let count=10;
+// for(let i=0;i<count;i++){
+//     m.push(+prompt(' enter a number dear : '))
+// }
+
+// const newm=[]
+// for(let i=count-1;i>=0;i--){
+//     newm.push(m[i]);
+    
+// }
+// console.log(m);
+// console.log(newm);
+
+
+
+//tamrin 2
+// let number=+prompt("Enter a number dear : ");
+// console.log(number);
+// let b,sum=0
+// while(number>0){
+//    b=number%10;
+//     sum=sum*10+b;
+//     number=parseInt(number/10)
+
+    
+// }
+
+// console.log(sum);
